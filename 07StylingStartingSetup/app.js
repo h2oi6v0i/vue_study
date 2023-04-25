4f61d2c6e381fe4d359fec6a517ca7180d52fa75
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-04-25 23:51:45 +0900

    [34] : 동적 클래스 :class

--- 07StylingStartingSetup/app.js
+++ 07StylingStartingSetup/app.js
@@ -8,16 +8,23 @@ const app = Vue.createApp({
     },
 
     methods: {
+        /** 토글 */
         boxSelected( box ) {
             if ( box === 'A' ) {
-                this.boxASelected = true;
+                this.boxASelected = !this.boxASelected;
             } else if ( box === 'B' ) {
-                this.boxBSelected = true;
+                this.boxBSelected = !this.boxBSelected;
             } else if ( box === 'C' ) {
-                this.boxCSelected = true;
+                this.boxCSelected = !this.boxCSelected;
             }
         }
     },
 });
 
 app.mount( '#styling' );
+
+/**
+ * HTML 설명
+ * 해당 프로퍼티 이름은 CSS 클래스를 반영하고
+ * 그 프로퍼티의 값은 true/false를 판단하여 class의 추가 여부를 보여준다.
+ */
