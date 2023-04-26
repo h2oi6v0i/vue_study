3b3ac06c90c1e52a15fdda27398287ae4dc7e403
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-04-27 00:23:00 +0900

    [37-38] : computed, v-model, :class

---
+++ 08BasicsAssignmentProblem/app.js
@@ -0,0 +1,31 @@
+const app = Vue.createApp({
+    data() {
+        return {
+           inputClass           : '',
+           isVisible            : true,
+           inputBackgroundColor : '',
+        }
+
+    },
+
+    computed: {
+        paraClasses() {
+            return {
+                user1   : this.inputClass === 'user1',
+                user2   : this.inputClass === 'user2',
+                visible : this.isVisible,
+                hidden  : !this.isVisible
+            }
+        }
+
+    },
+
+    methods: {
+        paragraphVisibility() {
+            this.isVisible = !this.isVisible;
+        }
+    },
+
+});
+
+app.mount( '#assignment' );
