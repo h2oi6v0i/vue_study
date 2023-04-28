605aea8752e78ddd2454bffbbfbbc981940f21a2
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-04-29 01:43:41 +0900

    [53] : 3턴마다 한 번씩 공격 가능한 스페셜 공격 기능 구현

--- 11PrjMonsterStartingSetup/app.js
+++ 11PrjMonsterStartingSetup/app.js
@@ -6,7 +6,8 @@ const app = Vue.createApp({
     data() {
         return {
             playerHealth  : 100,
-            monsterHealth : 100
+            monsterHealth : 100,
+            currentRound  : 0,
         }
     },
 
@@ -18,34 +19,53 @@ const app = Vue.createApp({
         playerBarStyles() {
             return { width : this.playerHealth + '%' };
         },
+
+        mayUseSpecialAttack() {
+            return this.currentRound % 3 !== 0;
+        }
     },
 
 
     methods: {
-        /** 몬스터 공격하기 */
+        /**
+         * 몬스터 공격하기
+         */
         attackMonster() {
+            this.currentRound++;
+            /**
+             * 피해량은 최소 5 최대 12
+             * Math.floor() : 소수점 아래 숫자 버림
+             * 마지막에 최솟값 다시 더하면 5~12 사이 임의의 숫자 산출 가능
+             */
             const attackValue = getRandomValue( 5, 12 );
             this.monsterHealth -= attackValue; 
             /** attackMonster가 발생하면 바로 attackPlayer가 트리거 되어야 한다. */
             this.attackPlayer(); // 모든 methods를 다 연결해야하는 건 아니라는 걸 배웠다.
         },
 
-        /** 플레이어 공격 당함 */
+        /**
+         * 플레이어 공격 당함
+         */
         attackPlayer() {
             const attackValue = getRandomValue( 8, 15 );
             this.playerHealth -= attackValue;
+        },
+
+        /**
+         * 몬스터 스페셜 공격하기
+         * - 3턴마다 한 번씩 공격 가능
+         * - 공격 불가능할 땐 버튼 비활성화
+         */
+        specialAttackMonster() {
+            this.currentRound++;
+            const attackValue = getRandomValue( 10, 25 );
+            this.monsterHealth -= attackValue; 
+            this.attackPlayer();
         }
     },
 });
 
 app.mount( '#game' );
-
- /**
-   * 피해량은 최소 5 최대 12
-   * Math.floor() : 소수점 아래 숫자 버림
-   * 마지막에 최솟값 다시 더하면 5~12 사이 임의의 숫자 산출 가능
-   */
-
 
  /**
   * this 쓰는 이유
