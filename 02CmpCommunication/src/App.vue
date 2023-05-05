25356c26b7469762a3520f0946dd0b978867e6c5
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-05-05 20:25:59 +0900

    [97-98] : 커스텀 이벤트($emit)

--- 02CmpCommunication/src/App.vue
+++ 02CmpCommunication/src/App.vue
@@ -4,21 +4,19 @@
       <h1>My Friends</h1>
     </header>
     <ul>
-      <!-- is-favorite 1 : true, 0 : false -->
-      <friend-contact
-        name="Manuel Lorenz"
-        phone-number="0123 45678 90"
-        email-address="manuel@localhost.com"
-        is-favorite="1"
-      >
-      </friend-contact>
-      <friend-contact
-        name="Julie Jones"
-        phone-number="0987 654421 21"
-        email-address="julie@localhost.com"
-        is-favorite="0"
-        >
-        </friend-contact>
+      <!-- is-favorite="true" -->
+      <!-- 이렇게 설정할 수 없다. 왜냐하면 저 자리엔 항상 텍스트가 와야 하기 때문이다. -->
+      <!-- 설정하고 싶으면 v-bind 사용해서 :is-favorite="true" 이렇게 써줘야 한다. -->
+      <friend-contact 
+        v-for="friend in friends"
+        :key="friend.id" 
+        :id="friend.id"
+        :name="friend.name"
+        :phone-number="friend.phone"
+        :email-address="friend.email"
+        :is-favorite="friend.isFavorite"
+        @toggle-favorite="toggleFavoriteStatus"
+      />
     </ul> 
   </section>
 </template>
@@ -33,16 +31,31 @@ export default {
           name: "Manuel Lorenz",
           phone: "0123 45678 90",
           email: "manuel@localhost.com",
+          isFavorite: true
         },
         {
           id: "julie",
           name: "Julie Jones",
           phone: "0987 654421 21",
           email: "julie@localhost.com",
+          isFavorite: false
         },
       ],
     };
   },
+
+  methods: {
+    /**
+     * toggleFavoriteStatus 메서드가 friendId를 전달받게 될 것이다.
+     * 인수 이름은 무엇이든 상관없고, 커스텀 이벤트를 발생시킬 때 FriendContact에서 제공한 두 번째 인수를 얻는다.
+     * toggleFavoriteStatus의 첫 번째 매개변수가 이 id인 것이다.
+     */
+    toggleFavoriteStatus( friendId ) {
+      const identifiedFriend = this.friends.find( ( friend ) => friend.id === friendId );
+      console.log( identifiedFriend );
+      identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+    }
+  }
 };
 </script>
 
--- 02CmpCommunication/src/components/FriendContact.vue
+++ 02CmpCommunication/src/components/FriendContact.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <h2>{{ name }} {{ friendIsFavorite === "1" ? "(Favorite)" : "" }}</h2>
+    <h2>{{ name }} {{ isFavorite ? '(favorite)' : '' }}</h2>
     <button @click="toggleFavorite">Toggle Favorite</button>
     <button @click="toggleDetails">
       {{ detailsAreVisible ? "Hide" : "Show" }} Details
@@ -20,58 +20,36 @@
 
 <script>
 export default {
-  /**
-   * props는 가장 단순하게 배열을 받는다. (무슨 말이야?)
-   * props로 정의한 것들은 data()와 마찬가지로 Vue 컴포넌트 전체에서 사용 가능하기 때문에 this 키워드로 참조 가능
-   * 그래서 대시를 쓰지 않는다. 대시를 사용하면 유효하지 않은 JavaScript 프로퍼티명이 되기 때문에 당연히 유효하지 않은 코드가 됨
-   * '카멜 케이스'로 정의된 프로퍼티를 Vue가 자동으로 해석해서 대시가 포함된 버전으로 바꿔준다.
-   * 즉, HTML 코드에서는 프로퍼티에 '케밥 케이스'를 사용하는 반면 컴포넌트가 받는 프로퍼티를 정의할 때는 '카멜 케이스'를 사용한다.
-   * props로 정의한 이름을 data나 computed에 쓸 수 없음
-   *
-   * - props는 불변한다.
-   */
-  // props: [ 'name', 'phoneNumber', 'emailAddress', 'isFavorite' ],
   props: {
+    id: {
+      type: String,
+      required: true,
+    },
     name: {
-      type : String,
-      required : true,
+      type: String,
+      required: true,
     },
 
-    phoneNumber : {
-      type : String,
-      required : true,
+    phoneNumber: {
+      type: String,
+      required: true,
     },
 
-    emailAddress : {
-      type : String,
-      required : true,
+    emailAddress: {
+      type: String,
+      required: true,
     },
 
-    isFavorite : {
-      type : String,
-      required : false,
-      /**
-       * 필수가 아닌 프로퍼티가 있다면 default 키를 추가할 수 있다.
-       * 프로퍼티가 설정되지 않았을 때의 기본값을 제공한다. (함수도 넣을 수 있음)
-       * ex) 컴포넌트의 기본값은 isFavorite = '0'
-       */
-      default : '0',
-      validator : function( value ) {
-        return value === '1' || value === '0';
-      }
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
     },
   },
 
   data() {
     return {
-      detailsAreVisible: false,
-      friend: {
-        id: "manuel",
-        name: "Manuel Lorenz",
-        phone: "0123 45678 90",
-        email: "manuel@localhost.com",
-      },
-      friendIsFavorite: this.isFavorite,
+      detailsAreVisible : false,
     };
   },
 
@@ -79,13 +57,18 @@ export default {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
+    /**
+     * 컴포넌트 내부에서만 바뀌는 것은 의미가 없기 때문에 자식에서 바뀐 데이터를 부모로 다시 되돌려줘야 한다.
+     * 컴포넌트가 부모에게 무언가 일어났음을 알리고자 한다면 컴포넌트는 부모가 수신할 이벤트를 발생시켜야 한다.
+     * $emit 메서드는 부모 컴포넌트에서 수신할 수 있는 커스텀 이벤트를 발생시켜 준다.
+     * - 최소한 하나의 인수인 커스텀 이벤트 이름을 적어줘야 한다.
+     * - 이외의 모든 추가 인수는 이벤트와 함께 전달될 데이터이다.
+     * - 케밥 케이스(케밥 케이스는 값을 전달할 때만 썼는데 이벤트도 마찬가지이다.)
+     * 컴포넌트가 이 커스텀 이벤트를 발생시키면 컴포넌트를 사용하는 App.vue 파일에서 이를 수신해야 한다.
+     */
 
     toggleFavorite() {
-      if (this.friendIsFavorite === "1") {
-        this.friendIsFavorite = "0";
-      } else {
-        this.friendIsFavorite = "1";
-      }
+      this.$emit( 'toggle-favorite', this.id );
     },
   },
 };
