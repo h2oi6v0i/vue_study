9f76a2ec972a1e7068e3b6baf9caf98815666c7d
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-05-11 01:21:34 +0900

    [120-123] : teleport, fragment

---
+++ 05CmpAdvStartingSetup/src/components/ErrorAlert.vue
@@ -0,0 +1,22 @@
+<template>
+  <dialog open>
+    <slot></slot>
+  </dialog>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+dialog {
+  position: fixed;
+  top: 20vh;
+  left: 30%;
+  width: 40%;
+  margin: 0;
+  padding: 1rem;
+  background-color: #fff;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+}
+</style>
--- 05CmpAdvStartingSetup/src/components/ManageGoals.vue
+++ 05CmpAdvStartingSetup/src/components/ManageGoals.vue
@@ -2,12 +2,48 @@
   <!-- 사용자가 새 강의 목표를 추가할 수 있는 컴포넌트 -->
   <div>
     <h2>Manage Goals</h2>
-    <input type="text" />
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <teleport to='body'>
+        <error-alert v-if="inputIsInvalid">
+        <h2>Input is invalid!</h2>
+        <p>Please enter at least a few characters...</p>
+        <button @click="confirmError">Okay</button>
+    </error-alert>
+    </teleport>
   </div>
 </template>
 
 <script>
-export default {};
+import ErrorAlert from "./ErrorAlert.vue";
+
+export default {
+  components: {
+    ErrorAlert,
+  },
+
+  data() {
+    return {
+      /** 처음에는 사용자가 input에 아무것도 입력하지 않았기 때문에 false로 초기화 */
+      inputIsInvalid: false,
+    };
+  },
+
+  methods: {
+    setGoal() {
+      /** 사용자가 입력한 값 받아오기 */
+      const enteredValue = this.$refs.goal.value;
+
+      if (enteredValue === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+
+    confirmError() {
+        this.inputIsInvalid = false;
+    }
+  },
+};
 </script>
 
 <style></style>
