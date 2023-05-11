ed54c91ee8f65acf9efc87c61b8074846c5ad2af
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-05-11 23:08:34 +0900

    [129] : props로 데이터 보내기

--- 06PrjCmpStartingSetup/src/App.vue
+++ 06PrjCmpStartingSetup/src/App.vue
@@ -1,21 +1,33 @@
 <template>
   <ul>
+    <learning-resource 
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title" 
+      :description="res.description"
+      :link="res.link"
+    />
   </ul>
-
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
 export default {
+  components: {
+    LearningResource 
+  },
+
   data() {
     return {
       storedResources: [
         {
-          id: 'official-guide',  /** 키 추가하거나 리소스를 삭제하는 기능을 위해서도 ID가 필요함 */
+          id: 'official-guide' /** 키 추가하거나 리소스를 삭제하는 기능을 위해서도 ID가 필요함 */,
           title: 'Official Guide',
           description: 'The official Vue.js documentation.',
           link: 'https://vuejs.org',
         },
-           {
+        {
           id: 'google',
           title: 'Google',
           description: 'Learn to google...',
