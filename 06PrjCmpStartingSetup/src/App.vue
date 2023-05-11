48c41c66e1554b1d44e3a96e262d8192c09d65c8
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-05-11 22:49:15 +0900

    [124-128] : 폴더 구조 변경 및 신규 프로젝트 생성

---
+++ 06PrjCmpStartingSetup/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+  </ul>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',  /** 키 추가하거나 리소스를 삭제하는 기능을 위해서도 ID가 필요함 */
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+           {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
