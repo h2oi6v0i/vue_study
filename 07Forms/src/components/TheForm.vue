f5fd1d3835b55d448de2569a11ffdd41f769b8fe
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-05-13 18:24:20 +0900

    [142-145][v-model] : select, checkbox, radio

--- 07Forms/src/components/TheForm.vue
+++ 07Forms/src/components/TheForm.vue
@@ -1,16 +1,22 @@
 <template>
-  <form>
+  <form @submit.prevent="submitForm">
     <div class="form-control">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" />
+      <input id="user-name" name="user-name" type="text" v-model="userName" />
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" />
+      <input
+        id="age"
+        name="age"
+        type="number"
+        v-model="userAge"
+        ref="ageInput"
+      />
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model="referrer">
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
@@ -19,38 +25,128 @@
     <div class="form-control">
       <h2>What are you interested in?</h2>
       <div>
-        <input id="interest-news" name="interest" type="checkbox" />
+        <input
+          id="interest-news"
+          name="interest"
+          type="checkbox"
+          value="news"
+          v-model="interest"
+        />
         <label for="interest-news">News</label>
       </div>
       <div>
-        <input id="interest-tutorials" name="interest" type="checkbox" />
+        <input
+          id="interest-tutorials"
+          name="interest"
+          type="checkbox"
+          value="tutorials"
+          v-model="interest"
+        />
         <label for="interest-tutorials">Tutorials</label>
       </div>
       <div>
-        <input id="interest-nothing" name="interest" type="checkbox" />
+        <input
+          id="interest-nothing"
+          name="interest"
+          type="checkbox"
+          value="nothing"
+          v-model="interest"
+        />
         <label for="interest-nothing">Nothing</label>
       </div>
     </div>
     <div class="form-control">
       <h2>How do you learn?</h2>
       <div>
-        <input id="how-video" name="how" type="radio" />
+        <input
+          id="how-video"
+          name="how"
+          type="radio"
+          value="video"
+          v-model="how"
+        />
         <label for="how-video">Video Courses</label>
       </div>
       <div>
-        <input id="how-blogs" name="how" type="radio" />
+        <input
+          id="how-blogs"
+          name="how"
+          type="radio"
+          value="blogs"
+          v-model="how"
+        />
         <label for="how-blogs">Blogs</label>
       </div>
       <div>
-        <input id="how-other" name="how" type="radio" />
+        <input
+          id="how-other"
+          name="how"
+          type="radio"
+          value="other"
+          v-model="how"
+        />
         <label for="how-other">Other</label>
       </div>
+    </div>
+    <div class="form-control">
+      <input
+        type="checkbox"
+        id="confirm-terms"
+        name="confirm-terms"
+        v-model="confirm"
+      />
+      <label for="confirm-terms">Agree to terms of use?</label>
     </div>
     <div>
       <button>Save Data</button>
     </div>
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: '',
+      userAge: null,
+      /** <option value="wom">Word of mouth</option>을 초깃값으로 설정 */
+      referrer: 'wom',
+      /** 빈 배열을 초깃값으로 설정하면 Vue가 사용자가 선택한 요소를 이 배열에 추가해 준다. */
+      interest: [],
+      confirm: false,
+      how: null,
+    };
+  },
+
+  methods: {
+    submitForm() {
+      console.log('유저 이름:', this.userName);
+      this.userName = ''; /** 사용자가 입력한 값 리셋 */
+
+      console.log('유저 나이');
+      console.log(this.userAge, typeof this.userAge);
+      console.log(this.$refs.ageInput.value, typeof this.$refs.ageInput.value);
+      console.log(typeof 31);
+      this.userAge = null; /** 사용자가 입력한 값 리셋 */
+
+      console.log('Referrer:', this.referrer);
+      this.referrer = 'wom'; /** 사용자가 입력한 값 리셋 */
+
+      console.log('Checkboxes');
+      console.log(this.interest);
+      this.interest = []; /** 사용자가 입력한 값 리셋 */
+
+      console.log('Radio buttons');
+      console.log(this.how);
+      this.how = null; /** 사용자가 입력한 값 리셋 */
+
+      console.log('Confirm?');
+      console.log(this.confirm);
+      this.confirm = false;
+    },
+  },
+};
+</script>
 
 <style scoped>
 form {
