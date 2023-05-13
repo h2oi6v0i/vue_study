97ae7213862c32e17018633ce4c0029e02b18b22
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-05-13 14:41:00 +0900

    [137][add-resource] : 유효성 검사, BaseDialog

---
+++ 06PrjCmpStartingSetup/src/components/UI/BaseDialog.vue
@@ -0,0 +1,88 @@
+<template>
+  <div @click="$emit('close')"></div>
+  <dialog open>
+    <header>
+      <slot name="header">
+        <!-- 사용자가 header를 지정하지 않았을 때 해당 기본 콘텐츠가 사용된다. -->
+        <h2>{{ title }}</h2>
+      </slot>
+    </header>
+    <section>
+      <slot></slot>
+    </section>
+    <menu>
+      <slot name="actions">
+        <base-button @click="$emit('close')">Close</base-button>
+      </slot>
+    </menu>
+  </dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      /** 커스텀 HTML 콘텐츠를 작성할 수도 있기 때문에 false */
+      required: false,
+    },
+  },
+
+  emits: ['close'],
+};
+</script>
+
+<style scoped>
+div {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.75);
+  z-index: 10;
+}
+
+dialog {
+  position: fixed;
+  top: 20vh;
+  left: 10%;
+  width: 80%;
+  z-index: 100;
+  border-radius: 12px;
+  border: none;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+}
+
+header {
+  background-color: #3a0061;
+  color: white;
+  width: 100%;
+  padding: 1rem;
+}
+
+header h2 {
+  margin: 0;
+}
+
+section {
+  padding: 1rem;
+}
+
+menu {
+  padding: 1rem;
+  display: flex;
+  justify-content: flex-end;
+  margin: 0;
+}
+
+@media (min-width: 768px) {
+  dialog {
+    left: calc(50% - 20rem);
+    width: 40rem;
+  }
+}
+</style>
--- 06PrjCmpStartingSetup/src/components/learning-resources/AddResource.vue
+++ 06PrjCmpStartingSetup/src/components/learning-resources/AddResource.vue
@@ -1,4 +1,16 @@
 <template>
+  <base-dialog v-if="inputIsInvalid" title="Invalid Input" @close="confirmError">
+    <template #default>
+      <p>Unfortunately, at least one input value is invalid.</p>
+      <p>
+        Please Check all inputs and make sure you enter at least a few
+        characters into each input field.
+      </p>
+    </template>
+    <template #actions>
+      <base-button @click="confirmError">Okay</base-button>
+    </template>
+  </base-dialog>
   <base-card>
     <form @submit.prevent="submitData">
       <div class="form-control">
@@ -7,7 +19,12 @@
       </div>
       <div class="form-control">
         <label for="description">Description</label>
-        <textarea id="description" name="description" rows="3" ref="descInput" />
+        <textarea
+          id="description"
+          name="description"
+          rows="3"
+          ref="descInput"
+        />
       </div>
       <div class="form-control">
         <label for="link">Link</label>
@@ -22,7 +39,14 @@
 
 <script>
 export default {
-  inject: [ 'addResource' ],
+  inject: ['addResource'],
+
+  data() {
+    return {
+      /** 처음에는 아무것도 입력되어 있지 않기 때문에 초깃값이 false이다. */
+      inputIsInvalid: false,
+    };
+  },
 
   methods: {
     /** input, textarea에 입력된 값 가져와서 전송하기 ( 방법1. v-model / 방법2. ref )*/
@@ -31,8 +55,27 @@ export default {
       const enteredDescription = this.$refs.descInput.value;
       const enteredUrl = this.$refs.linkInput.value;
 
-      this.addResource( enteredTitle, enteredDescription, enteredUrl );
+      /**
+       * 세 가지의 경우 중 하나라도 해당하면 입력값이 유효하지 않은 것으로 설정한다.
+       * 그리고 그 결과를 반환하여 아래의 리소스가 추가되지 않도록 막는다.
+       * 그 기능은 return이 한다. return 문은 함수 실행을 막기 때문이다.
+       */
+      if (
+        enteredTitle.trim() === '' ||
+        enteredDescription.trim() === '' ||
+        enteredUrl.trim() === ''
+      ) {
+        this.inputIsInvalid = true;
+        return;
+      }
+
+      this.addResource(enteredTitle, enteredDescription, enteredUrl);
     },
+
+    /** Dialog 닫기 */
+    confirmError() {
+      this.inputIsInvalid = false;
+    }
   },
 };
 </script>
