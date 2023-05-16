7d274b443a5da3cc970517ff918167980a869033
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-05-17 00:47:28 +0900

    [163-165][HTTP] : 오류 처리(2)

--- 08Http/src/components/survey/LearningSurvey.vue
+++ 08Http/src/components/survey/LearningSurvey.vue
@@ -26,9 +26,8 @@
           <input type="radio" id="rating-great" value="great" name="rating" v-model="chosenRating" />
           <label for="rating-great">Great</label>
         </div>
-        <p
-          v-if="invalidInput"
-        >One or more input fields are invalid. Please check your provided data.</p>
+        <p v-if="invalidInput">One or more input fields are invalid. Please check your provided data.</p>
+        <p v-if="error">{{ error }}</p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -44,6 +43,7 @@ export default {
       enteredName: '',
       chosenRating: null,
       invalidInput: false,
+      error: null
     };
   },
 
@@ -62,7 +62,9 @@ export default {
       //   rating: this.chosenRating,
       // });
 
-      /** fetch( 이 URL에서 데이터를 가져옴 , 요청을 구성할 수 있는 JS 객체 ) */
+      this.error = null;
+
+      /** fetch( 이 URL에서 데이터를 가져옴 , 요청을 구성할 수 있는 JS 객체 ) */      
       fetch( 'https://vue-http-demo-dae75-default-rtdb.firebaseio.com/surveys.json', {
         method: 'POST',
         /** Content-Type 헤더를 application/json으로 설정해서 JSON 형식의 일부 데이터가 요청에 추가된 걸 서버에 알린다. */
@@ -71,7 +73,17 @@ export default {
         },
         /** 과거에 발생시킨 값이 이 객체에 추가된다. 그리고 JS 객체를 JSON 형식으로 변환한다. */
         body: JSON.stringify( { name: this.enteredName, rating: this.chosenRating } ),
-      } );
+      }).then( response => {
+        if ( response.ok) {
+          // ....
+        } else {
+          /** 새 오류 객체 발생 : 새로운 오류를 발생시켜서 자동으로 catch 블록이 이를 인식하게 만듦 */
+          throw new Error( 'Could not save data!' );
+        }
+      }).catch(error => {
+        console.log(error);
+        this.error = error.message;
+      });
 
       this.enteredName = '';
       this.chosenRating = null;
