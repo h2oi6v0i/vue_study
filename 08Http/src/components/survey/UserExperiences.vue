2869fd035c734ac5eddeef80ccd2bd46a0388ab6
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-05-16 23:57:47 +0900

    [158][HTTP] :  데이터 가져오기(GET 요청) 및 응답 데이터 변환

--- 08Http/src/components/survey/UserExperiences.vue
+++ 08Http/src/components/survey/UserExperiences.vue
@@ -3,7 +3,7 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
       <ul>
         <survey-result
@@ -21,9 +21,47 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
+  },
+
+  data() {
+    return {
+      results: [],
+    };
+  },
+
+  methods: {
+    loadExperiences() {
+      fetch(
+        'https://vue-http-demo-dae75-default-rtdb.firebaseio.com/surveys.json'
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          // this.results = data; data는 id 프로퍼티로 이루어진 객체이기 때문에 이렇게 작업하면 안 됨
+
+          /** 1. results라는 상수를 만든 뒤 빈 배열을 지정한다. */
+          const results = [];
+
+          /** 2. data 내의 모든 id를 검색하도록 설정한다. */
+
+          for (const id in data) {
+            /** 3. 새로운 객체를 results 배열에 푸시한다. */
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+
+          /** 4. data()의 임시 results를 results와 같다고 설정한다. */
+          this.results = results;
+        });
+    },
   },
 };
 </script>
