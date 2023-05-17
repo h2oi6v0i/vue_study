5d4ed3aefa3279a0b29e76983c201d14b7fc57bf
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-05-18 00:38:58 +0900

    [173-175][Routing] : 동적 세그먼트, 내비게이션 및 동적 경로

--- 09Routing/src/components/teams/TeamMembers.vue
+++ 09Routing/src/components/teams/TeamMembers.vue
@@ -16,18 +16,40 @@
 import UserItem from '../users/UserItem.vue';
 
 export default {
+  inject: [ 'teams', 'users' ],
+
   components: {
     UserItem
   },
+
   data() {
     return {
-      teamName: 'Test',
-      members: [
-        { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
-        { id: 'u2', fullName: 'Max Schwarz', role: 'Engineer' },
-      ],
-    };
+      teamName: '',
+      members: [],
+    }
   },
+
+  created() {
+    /** 1. teamId 가져와서 추가 상수에 저장하기 */
+    const teamId = this.$route.params.teamId;
+
+    /** 2. 주입(inject)된 teams 배열에서 teamId를 가지고 올바른 팀 찾기 */
+    const selectedTeam = this.teams.find( team => team.id === teamId );
+
+    /** 3. 팀을 찾은 후, 팀 소속 멤버 찾기 */
+    const members = selectedTeam.members; 
+
+    /** 4. 주입(inject)된 users 배열에서 해당하는 members의 전체 사용자 엔트리를 찾는다. */
+    const selectedMembers = [];
+
+    for ( const member of members ) {
+      const selectedUser = this.users.find( user => user.id === member );
+      selectedMembers.push( selectedUser );
+    }
+
+    this.members = selectedMembers;
+    this.teamName = selectedTeam.name;
+  }
 };
 </script>
 
