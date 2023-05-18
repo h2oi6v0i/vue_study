67d9e87c425c144b8cfd75ff6d99c67d9d1c8e91
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-05-19 00:43:38 +0900

    [213-215] : state(), Mutations

--- 11Vuex/src/main.js
+++ 11Vuex/src/main.js
@@ -1,7 +1,25 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex';
 
 import App from './App.vue';
 
-const app = createApp(App);
+const store = createStore({
+    state() {
+        return {
+            counter: 0
+        }
+    },
 
-app.mount('#app');
+    mutations: {
+        increment( state ) {
+            state.counter += 2;
+        }
+    }
+});
+
+const app = createApp( App );
+
+/** app.use를 호출해서 저장소를 전달한다. */
+app.use( store );
+
+app.mount( '#app' );
