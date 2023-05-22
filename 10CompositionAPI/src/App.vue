e8f24be5c2239b5830d1d86643da2ad5246ae090
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-05-22 23:44:19 +0900

    [284-287] : setup(), ref

--- 10CompositionAPI/src/App.vue
+++ 10CompositionAPI/src/App.vue
@@ -4,14 +4,15 @@
   </section>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      userName: 'Maximilian',
-    };
-  },
-};
+<script setup>
+import { ref } from 'vue';
+
+const userName = ref('Gary');
+
+setTimeout(function () {
+  // uName = 'Yujung'; uName을 let으로 바꿔서 작업해도 안 됨 왜냐하면 ref를 덮어쓰기 때문에 반응성을 잃음
+  userName.value = 'My Hyuk';
+}, 2000);
 </script>
 
 <style>
