535af82a837ffecf885ce291626beced33095992
Jeon Yujung <h2oi6v0i.@gmail.com>
2023-06-16 00:16:01 +0900

    [290] : 메서드를 일반 함수로 바꾸기

--- 10CompositionAPI/src/App.vue
+++ 10CompositionAPI/src/App.vue
@@ -1,26 +1,27 @@
 <template>
   <section class="container">
-    <h2>{{ user.name }}</h2>
-    <h3>{{ user.age }}</h3>
+    <h2>{{ userName }}</h2>
+    <h3>{{ userAge }}</h3>
+    <button @click="setAge">Change Age</button>
   </section>
 </template>
 
-<script setup>
-import { reactive, isReactive, isRef } from 'vue';
+<script>
+import { ref } from 'vue';
 
-const user = reactive({
-  name : 'Gary',
-  age  : 14,
-});
+export default {
+  setup() {
+    const uName = ref('Yujung');
+    const uAge = ref(28);
 
-setTimeout( function () {
-  // uName = 'Yujung'; uName을 let으로 바꿔서 작업해도 안 됨 왜냐하면 ref를 덮어쓰기 때문에 반응성을 잃음
-  user.name = 'Yujung';
-  user.age = 28;
+    function setNewAge() {
+      uAge.value = 12;
+    }
 
-  console.log( isReactive( user ) );
-  console.log( isRef( user ) );
-}, 1000 );
+    /** setup() 메서드에서 정의하고 객체로 반환한다. */
+    return { userName: uName, userAge: uAge, setAge: setNewAge };
+  },
+};
 </script>
 
 <style>
